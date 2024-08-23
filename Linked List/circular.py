68dc8c4331ba2dcaa0549c7906be044aea340b57
Anmol Arora <heymailme04@gmail.com>
2024-08-23 17:49:32 +0530

    Update circular.py

--- Linked List/circular.py
+++ Linked List/circular.py
@@ -8,3 +8,25 @@ class CLL:
         self.last = last
     def is_empty(self):
         return self.last==None
+    
+    def at_start(self,data):
+        n = Node(data)
+        if self.is_empty():
+            n.next = n
+            self.last = n
+        else:
+            n.next = self.last.next
+            self.last.next = n
+    
+    def at_last(self,data):
+        n = Node(data)
+        if self.is_empty():
+            n.next = n
+            self.last = n
+        else:
+            n.next = self.last.next
+            self.last.next = n
+            self.last = n
+              
+            
+        
