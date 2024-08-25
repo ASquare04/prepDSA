e13445651b25050b6f5f6b3317ffda86ff877cb1
Anmol Arora <heymailme04@gmail.com>
2024-08-25 11:04:13 +0530

    Update Stacks.py

--- Stacks.py
+++ Stacks.py
@@ -19,3 +19,49 @@ def pop():
         x = stack[top]
         top -= 1 
         return x
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+        
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def push(self,data):
+        self.items.append(data)
+        
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Stack Is Empty")
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Stack Is Empty")
+        
+    def size(self):
+        return len(self.items)
+    
+    def print_stack(self):
+        print(self.items)
+        
+
+S = Stack()
+
+S.push(5)
+S.push(15)
+S.push(25)
+S.print_stack()
+print(S.pop())
+S.print_stack()
+print(S.peek())
+            
